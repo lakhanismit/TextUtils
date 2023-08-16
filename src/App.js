8426b4f3e11f8e6f442b140a689f5264764611a2
lakhanismit <smitlakhani2062002@gmail.com>
2023-08-16 15:23:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
+import Navrbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import About from './components/About';
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+  const showAlert = (msg, type) => {
+    setAlert({
+      msg: msg,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+  const toggleDarkModeBtn = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = 'black'
+      showAlert("Black Dark mode Enable", "success")
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode Enable", "success")
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navrbar title='TextUtils' mode={mode} toggleDarkModeBtn={toggleDarkModeBtn} />
+        <Alert alert={alert} />
+        <div className="container my-4">
+          <Routes>
+            <Route exact path="/" element={<TextForm heading="Enter your text here :-" mode={mode} showAlert={showAlert} />} />
+            <Route exact path="/about" element={<About mode={mode} toggleDarkModeBtn={toggleDarkModeBtn} />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const [text, setText] = useState('')
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+    const chageToUP = () => {
+        let newText = text.toUpperCase()
+        setText(newText)
+        props.showAlert('Changed to Uppercase', 'success')
+    }
+    const chageToLow = () => {
+        let newText = text.toLowerCase()
+        setText(newText)
+        props.showAlert('Changed to Lowercase', 'success')
+    }
+    const chageClear = () => {
+        let newText = ''
+        setText(newText)
+        props.showAlert('Text cleared', 'success')
+    }
+    const copyAll = () => {
+        let copyText = document.getElementById('myform')
+        copyText.select()
+        navigator.clipboard.writeText(copyText.value)
+        props.showAlert('Text copied to clipboard', 'success')
+    }
+    const extraSpaces = () => {
+        let newText = text.split(/[ ]+/)
+        setText(newText.join(' '))
+        props.showAlert('Removed extra spaces', 'success')
+    }
+
+    return (
+        <>
+            <div style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+                <div>
+                    <div className="mb-3">
+                        <label htmlFor="myform" className="form-label">{props.heading}</label>
+                        <textarea className="form-control" value={text} onChange={handleOnChange} id="myform" rows="8" style={{ backgroundColor: props.mode === 'dark' ? 'black' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }}></textarea>
+                        <button className="btn btn-primary m-1" onClick={chageToUP}>Change To UpperCase</button>
+                        <button className="btn btn-primary m-1" onClick={chageToLow}>Change To LowerCase</button>
+                        <button className="btn btn-primary m-1" onClick={chageClear}>Clear Texts</button>
+                        <button className="btn btn-primary m-1" onClick={copyAll}>Copy Texts</button>
+                        <button className="btn btn-primary m-1" onClick={extraSpaces}>Remove Extra Spaces</button>
+                    </div>
+                </div>
+                <h2>Text Summery</h2>
+                <table className={`table table-${props.mode}`}>
+                    <thead>
+                        <tr>
+                            <th scope="col">Number of Words</th>
+                            <th scope="col">Number of Charachers</th>
+                            <th scope="col">Number of Sentances</th>
+                            <th scope="col">Number of Charachers per Word</th>
+                            <th scope="col">Number of Words per Sentence</th>
+                            <th scope="col">Number of Paragraphs</th>
+                            <th scope="col">Number of Sentances per Paragraph</th>
+                            <th scope="col">How much Time to read (in minumtes)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{text.split(' ').length}</td>
+                            <td>{text.length}</td>
+                            <td>{text.split('.').length - 1}</td>
+                            <td>{(text.length) / (text.split(' ').length)}</td>
+                            <td>{(text.split(' ').length) / (text.split('.').length - 1)}</td>
+                            <td>{text.replace(/\n$/gm, '').split(/\n/).length}</td>
+                            <td>{(text.split('.').length - 1) / (text.replace(/\n$/gm, '').split(/\n/).length)}</td>
+                            <td>{0.008 * text.split(' ').length}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <h2>Perview</h2>
+                <p>{text.length > 0 ? text : 'Please Enter text to show perview'}</p>
+            </div>
+        </>
+    )
+}
